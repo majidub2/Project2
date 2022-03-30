8f1e3e661c1357347f22a5a65876466a8ff86471
majidub2 <majid.mohamed9802@gmail.com>
2022-03-30 15:55:45 +0000

    worked on dev branch

--- service4/tests/test_accountnumber.py
+++ service4/tests/test_accountnumber.py
@@ -10,7 +10,6 @@ class TestBase(TestCase):
         return app
 
 
-
 class TestResponse(TestBase):
    
     def test_get_accountnumber(self):
@@ -20,14 +19,4 @@ class TestResponse(TestBase):
         test_json = b'{"account_number": "ABC1000", "prize": "0"}'
         assert json_response == test_json
 
-        # with patch('random.randrange') as random:
-        #     random.return_value = "1000"
-        #     data = {"letters" : 'ABC', "numbers" : '1000'}
-        #     response = self.client.post(url_for('prize'), json=data)
-        #     test_json = b'{"account_number": "AB14CDG", "prize": "blue"}'
-        
-        # data = {"letters" : 'ABCDE', "numbers" : '4'}
-        # response = self.client.post(url_for('get_numberplate'), json=data)
-        # json_response = response.data
-        # test_json = b'{"account_number": "AB04CDE", "prize": "yellow"}'
-        # assert json_response == test_json
+     
