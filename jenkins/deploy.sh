30818b44b5471b49cc8d98b007e35b4c38ea38c3
majidub2 <majid.mohamed9802@gmail.com>
2022-04-01 16:21:25 +0000

    fixing deploy stack

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,3 +1,3 @@
 export MYSQL_ROOT_PASSWORD
-docker stack deploy --compose-file docker-compose.yaml animal-noises-stack
-docker service update --replicas 3 animal-noises-stack_front-end
+docker stack deploy --compose-file docker-compose.yaml finalproject-stack
+docker service update --replicas 3 finalproject-stack_front-end
