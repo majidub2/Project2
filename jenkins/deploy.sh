d7c441df15c456acac267ae5603776257af5fff1
majidub2 <majid.mohamed9802@gmail.com>
2022-04-04 15:33:19 +0000

    final push in branch

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,6 +1,5 @@
 export MYSQL_ROOT_PASSWORD=hellomajid
 docker stack deploy --compose-file docker-compose.yaml finalproject-stack
-# docker service update --replicas 3 finalproject-stack
 
 docker service update --replicas 3 finalproject-stack_accountnumber
 docker service update --replicas 3 finalproject-stack_frontend
