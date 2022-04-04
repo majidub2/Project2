48c62dc79a8668b09c015abd554cda7cb5643b65
majidub2 <majid.mohamed9802@gmail.com>
2022-04-04 13:05:11 +0000

    rolling update

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,3 +1,7 @@
 export MYSQL_ROOT_PASSWORD
 docker stack deploy --compose-file docker-compose.yaml finalproject-stack
-# docker service update --replicas 3 finalproject-stack_frontend
+# docker service update --replicas 3 finalproject-stack
+docker service update --replicas 3 finalproject-stack_accountnumber
+docker service update --replicas 3 finalproject-stack_frontend
+docker service update --replicas 3 finalproject-stack_letters
+docker service update --replicas 3 finalproject-stack_numbers
