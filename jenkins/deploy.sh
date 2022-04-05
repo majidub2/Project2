b562c15932e7405f5c284acee9d24913b8be9fd4
majidub2 <majid.mohamed9802@gmail.com>
2022-04-05 18:27:09 +0000

    testing webhook

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,4 +1,4 @@
-export MYSQL_ROOT_PASSWORD= ${MYSQL_ROOT_PASSWORD}
+export MYSQL_ROOT_PASSWORD= .
 docker stack deploy --compose-file docker-compose.yaml finalproject-stack
 
 docker service update --replicas 3 finalproject-stack_accountnumber
